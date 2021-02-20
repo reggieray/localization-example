710dc90f460740708cc55972cc34c3e7f0b9c811
matthew regis <regis12@gmail.com>
2021-02-20 21:44:55 +0000

    init

--- Localization.Example/Controllers/WeatherForecastController.cs
+++ Localization.Example/Controllers/WeatherForecastController.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Localization.Example.Resources;
+using Localization.Example.Weather;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace Localization.Example.Controllers
 {
@@ -17,21 +18,29 @@ namespace Localization.Example.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeatherForecastController"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
         }
-
+        /// <summary>
+        /// Gets the weather forecast.
+        /// </summary>
+        /// <param name="lang">The language. Support languages en-US, es-ES, zh-CN</param>
+        /// <returns></returns>
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<WeatherForecast> Get([FromQuery] string lang = WeatherLocale.DefaultLocale)
         {
+            var cultureInfo = WeatherLocale.GetLocale(lang);
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = WeatherSummaries.ResourceManager.GetString(Summaries[rng.Next(Summaries.Length)], cultureInfo)
             })
             .ToArray();
         }
---
+++ Localization.Example/Weather/WeatherLocale.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Localization.Example.Weather
+{
+    public static class WeatherLocale
+    {
+        public const string DefaultLocale = "en-US";
+
+        public static CultureInfo GetLocale(string lang)
+        {
+            try
+            {
+                return new CultureInfo(lang);
+            }
+            catch (CultureNotFoundException)
+            {
+                return new CultureInfo(DefaultLocale);
+            }
+        }
+    }
+}
